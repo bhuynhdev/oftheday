d5b090d2e9f9ab95e07edd50c3aff2ba8c38a45c
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2023-11-05 10:21:10 -0500

    Try redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import Image from "next/image";
+import { getSession } from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession();
+
+  if (session) {
+    return redirect("/marketplace");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <a
-        href="/api/auth/login?returnTo=/marketplace"
-        className="rounded-xl bg-rose-600 px-8 py-4 text-2xl"
-      >
+      <a href="/api/auth/login" className="rounded-xl bg-rose-600 px-8 py-4 text-2xl">
         Login
       </a>
     </main>
